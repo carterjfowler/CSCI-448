3cd8486cc1736228eb9be46d69728f170fd66522
carterjfowler <carterjfowler@gmail.com>
2020-01-29 14:49:04 -0700

    FINISHED Lab02Bgit statusgit status!

--- AndroidStudioProjects/GeoQuiz/app/src/main/java/com/csci448/carterjfowler/QuizActivity.kt
+++ AndroidStudioProjects/GeoQuiz/app/src/main/java/com/csci448/carterjfowler/QuizActivity.kt
@@ -2,21 +2,61 @@ package com.csci448.carterjfowler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
     private lateinit var scoreTextView: TextView
+    private lateinit var questionTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
         scoreTextView = findViewById( R.id.score_text_view )
+        questionTextView = findViewById( R.id.question_text_view)
+
+        val trueButton: Button = findViewById( R.id.true_button )
+        val falseButton: Button = findViewById( R.id.false_button )
+        trueButton.setOnClickListener { checkAnswer(true) }
+        falseButton.setOnClickListener { checkAnswer(false) }
+
+        val previousButton: Button = findViewById( R.id.previous_button )
+        val nextButton: Button = findViewById( R.id.next_button )
+        previousButton.setOnClickListener { moveToQuestion(1)}
+        nextButton.setOnClickListener { moveToQuestion(-1) }
 
         updateQuestion();
     }
 
     fun updateQuestion() {
+        setCurrentScoreText()
+        questionTextView.text = resources.getString( QuizMaster.currentQuestionTextId )
+    }
+
+    private fun setCurrentScoreText() {
         scoreTextView.text = QuizMaster.currentScore.toString()
     }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        if (QuizMaster.isAnswerCorrect(userAnswer)) {
+            Toast.makeText(baseContext, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            setCurrentScoreText()
+        } else {
+            Toast.makeText(baseContext, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun moveToQuestion(direction: Int) {
+        if (direction > 0) {
+            QuizMaster.moveToNextQuestion()
+        } else if (direction < 0) {
+            QuizMaster.moveToPreviousQuestion()
+        }
+        updateQuestion()
+    }
+
 }
+
--- AndroidStudioProjects/GeoQuiz/app/src/main/java/com/csci448/carterjfowler/QuizMaster.kt
+++ AndroidStudioProjects/GeoQuiz/app/src/main/java/com/csci448/carterjfowler/QuizMaster.kt
@@ -1,11 +1,19 @@
 package com.csci448.carterjfowler
 
 object QuizMaster {
-    private val theQuestion = Question(R.string.question1, false)
+    private val questionBank: MutableList<Question> = mutableListOf()
     private var score = 0
+    private var currentQuestionIndex = 0;
+
+    init {
+        questionBank.add( Question(R.string.question1, false) )
+        questionBank.add( Question(R.string.question2, true) )
+        questionBank.add( Question(R.string.question3, true) )
+        questionBank.add( Question(R.string.question4, false) )
+    }
 
     private val currentQuestion: Question
-        get() = theQuestion
+        get() = questionBank[currentQuestionIndex]
 
     val currentQuestionTextId: Int
         get() = currentQuestion.textResId
@@ -20,6 +28,21 @@ object QuizMaster {
             return true
         } else {
             return false
+        }
+    }
+
+    fun moveToNextQuestion() {
+        if (currentQuestionIndex < questionBank.size - 1) {
+            ++currentQuestionIndex
+        } else {
+            currentQuestionIndex = 0
+        }
+    }
+    fun moveToPreviousQuestion() {
+        if (currentQuestionIndex > 0) {
+            --currentQuestionIndex
+        } else {
+            currentQuestionIndex = questionBank.size - 1
         }
     }
 }
